793e78d9ecf7606d1639b8947bc7b3722f339049
guilhermeaferrari <guile.ferrari@hotmail.com>
2023-07-12 18:16:43 -0300

    feat: added dbcontext

---
+++ SampleEFCore-ProductsManagement/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleEFCore_ProductsManagement.Domain;
+
+namespace SampleEFCore_ProductsManagement.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost,1433;
+                Database=EFCore_ProductsManagement;
+                User ID=sa;Password=1q2w3e4r@#$;
+                TrustServerCertificate=true");
+
+        }
+    }
+}
