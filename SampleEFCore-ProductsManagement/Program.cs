194e36aacea41e76c79b0601011ddbff6fca5101
guilhermeaferrari <guile.ferrari@hotmail.com>
2023-07-12 22:52:41 -0300

    feat: added migrations

--- SampleEFCore-ProductsManagement/Program.cs
+++ SampleEFCore-ProductsManagement/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using SampleEFCore_ProductsManagement.Data;
+
 Console.WriteLine("Hello, World!");
+
+//var dbContext = new ApplicationContext();
+//dbContext.Database.Migrate();
